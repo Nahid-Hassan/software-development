ffd2ace610a5116d4c8f7f94f221652de88dd7af
Nahid-Hassan <nahid.cseru@gmail.com>
2021-05-02 19:20:55 +0600

    update valid parentheses

--- py-codelab/leetcode/valid-parentheses.py
+++ py-codelab/leetcode/valid-parentheses.py
@@ -1,16 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
         stack = list()
-        
         for ch in s:
             if ch == '(' or ch == '{' or ch == '[':
                 stack.append(ch)
             else:
-                p = stack.pop()
-                if (ch == ')' and p != '(') or (ch == '}' and p != '{') or (ch == ']' and p != '['):
+                if len(stack) != 0:
+                    p = stack.pop()
+                    if (p == '(' and ch != ')') or (p == '{' and ch != '}') or (p == '[' and ch != ']'):
+                        return False
+                else:
                     return False
         
-        if len(stack) != 0:
-            return False
-        else:
+        if len(stack) == 0:
             return True
+        else:
+            return False
